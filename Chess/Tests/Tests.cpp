f73bbe1038bca107bcff246428303cbff135e722
Nikolaus Walton <nikawalton2@gmail.com>
2023-03-06 15:56:32 -0800

    Write unit tests for GamePiece

--- Chess/Tests/Tests.cpp
+++ Chess/Tests/Tests.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "../Chess/GamePiece.h"
+#include "../Chess/GamePiece.cpp"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests
@@ -9,8 +12,20 @@ namespace Tests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_GamePieceClass)
 		{
+			GamePiece *test_piece = new GamePiece();
+
+			//test default constructor
+			Assert::AreEqual((int)color::kNone, (int)test_piece->GetColor());
+			Assert::AreEqual((int)type::kNone, (int)test_piece->GetType());
+
+			//test setters
+			test_piece->SetColor(color::kBlack);
+			Assert::AreEqual((int)color::kBlack, (int)test_piece->GetColor());
+
+			test_piece->SetType(type::kQueen);
+			Assert::AreEqual((int)type::kQueen, (int)test_piece->GetType());
 		}
 	};
 }
